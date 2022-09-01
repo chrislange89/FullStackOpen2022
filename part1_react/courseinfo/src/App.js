0c60bfd08dfea5b908df6c547543248b65d96f37
Chris Lange <chris_lange_89@hotmail.com>
2022-09-02 08:15:06 +1000

    complete excersize 1.4

--- part1_react/courseinfo/src/App.js
+++ part1_react/courseinfo/src/App.js
@@ -11,44 +11,43 @@ const Part = (props) => {
 };
 
 const Content = (props) => {
-  console.log(props);
+  // assume there are only three items
   return (
     <div>
-      <Part part={props.part1} />
-      <Part part={props.part2} />
-      <Part part={props.part3} />
+      <Part key={props.parts[0].name} part={props.parts[0]}/>
+      <Part key={props.parts[1].name} part={props.parts[1]}/>
+      <Part key={props.parts[2].name} part={props.parts[2]}/>
     </div>
   );
 };
 
 const Total = (props) => {
-  return <p>Number of exercises {props.total}</p>;
+  //assume only three parts
+  return <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>;
 };
 
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10,
-  };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7,
-  };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14,
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+    },
+  ]
 
   return (
     <div>
       <Course course={course} />
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-      />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };

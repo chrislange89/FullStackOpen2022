b2e108ac4619403a64bf985fcf727405b27a8963
Chris Lange <chris_lange_89@hotmail.com>
2022-09-02 13:28:06 +1000

    complete 2.8

--- part2_more_react/phonebook/src/App.js
+++ part2_more_react/phonebook/src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  ]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [searchValue, setSearchValue] = useState('');
+
+  function handleNameChange(event) {
+    setNewName(event.target.value);
+  }
+
+  function handleNumberChange(event) {
+    setNewNumber(event.target.value);
+  }
+
+  function handleSearchChange(event) {
+    setSearchValue(event.target.value);
+  }
+
+  function handleAddNewName(event) {
+    event.preventDefault();
+    const nameExists = persons.find((person) => person.name === newName);
+    if (!nameExists) {
+      let newPerson = {
+        name: newName,
+      };
+      setPersons(persons.concat(newPerson));
+      setNewName('');
+      return;
+    }
+
+    alert(`Cannot add ${newName}, this person already exists!`);
+  }
+
+  const peopleToShow = persons.filter((person) => person.name.includes(searchValue));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <div>filter shown with <input value={searchValue} onChange={handleSearchChange}/></div>
+      <h2>add a new</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type='submit' onClick={handleAddNewName}>
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Contacts</h2>
+      <ul>
+        {peopleToShow.map((person) => (
+          <li key={person.id}>
+            {person.name} - {person.number}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;

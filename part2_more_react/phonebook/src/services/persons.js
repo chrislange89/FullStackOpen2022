69e980c7b87ce9d0434c5270200b99a2c6d4b62b
Chris Lange <chris_lange_89@hotmail.com>
2022-09-08 13:05:07 +1000

    add empty line after import

--- part2_more_react/phonebook/src/services/persons.js
+++ part2_more_react/phonebook/src/services/persons.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+
 const url = 'https://localhost:3001/persons';
 
 function getAll() {
@@ -6,11 +7,11 @@ function getAll() {
 }
 
 function create(newPerson) {
-  return axios.post(url, newPerson)
+  return axios.post(url, newPerson);
 }
 
 function update(id, newPerson) {
-  return axios.put(`${url}/${id}`, newPerson)
+  return axios.put(`${url}/${id}`, newPerson);
 }
 
 export default { getAll, create, update };
